5bd53baf687e18737b336734737cca573e63c141
Programming Hero <programming.hero1@gmail.com>
2024-12-10 20:49:55 -0600

    bad way to connect two collection

--- index.js
+++ index.js
@@ -52,6 +52,19 @@ async function run() {
             const email = req.query.email;
             const query = { applicant_email: email }
             const result = await jobApplicationCollection.find(query).toArray();
+
+            // fokira way to aggregate data
+            for (const application of result) {
+                console.log(application.job_id)
+                const query1 = { _id: new ObjectId(application.job_id) }
+                const job = await jobsCollection.findOne(query1);
+                if(job){
+                    application.title = job.title;
+                    application.company = job.company;
+                    application.company_logo = job.company_logo;
+                }
+            }
+
             res.send(result);
         })
 
