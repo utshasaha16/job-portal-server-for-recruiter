3b68b7e16a6f89368fe68fa503283c55a0800898
Programming Hero <programming.hero1@gmail.com>
2024-12-11 22:18:59 -0600

    get applications for a job

--- index.js
+++ index.js
@@ -81,6 +81,15 @@ async function run() {
             res.send(result);
         })
 
+        // app.get('/job-applications/:id') ==> get a specific job application by id
+
+        app.get('/job-applications/jobs/:job_id', async (req, res) => {
+            const jobId = req.params.job_id;
+            const query = { job_id: jobId }
+            const result = await jobApplicationCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post('/job-applications', async (req, res) => {
             const application = req.body;
             const result = await jobApplicationCollection.insertOne(application);
