14de88ec3e78429b50c31a636d41a8f963e0b990
utshasaha16 <utsharoy12345@gmail.com>
2024-12-18 21:00:22 +0600

    install jwt and cookie

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = ('cookie-parser')
 const cors = require('cors');
 const app = express();
-require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwlha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,9 +26,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         // jobs related apis
@@ -57,6 +60,10 @@ async function run() {
             res.send(result);
         })
 
+        // auth related apis
+        app.post('/jwt', (req, res) => {
+            
+        })
 
         // job application apis
         // get all data, get one data, get some data [o, 1, many]
